55d59a04e4d8af5ba13b01b56f2c54073941707d
8503376 <8503376@HTEJZ-NTBTASY04>
2022-04-27 11:23:02 -0300

    Adicionar arquivos de projeto.

---
+++ Example1/logiciel1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    public class logiciel1
+    {
+
+        public static void Main(String[] args)
+        {
+        
+        String prenom;
+        int age;
+
+        Console.WriteLine("Tapez le prénom: ");
+        prenom=Console.ReadLine();
+
+        Console.WriteLine("Tapez l\'agê de la personne: ");
+        age=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Salut {prenom} ton agê est {age}");
+
+        Console.ReadKey();
+        }
+    }
+}
